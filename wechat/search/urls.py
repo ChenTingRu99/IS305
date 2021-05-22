f548b3fc9e4063ea8e72a210796d3e92e252bda0
ChenTingRu99 <61975323+ChenTingRu99@users.noreply.github.com>
2021-05-22 18:18:12 +0800

    登陆和注册

--- wechat/search/urls.py
+++ wechat/search/urls.py
@@ -2,4 +2,7 @@ from . import views
 from django.urls import path
 urlpatterns = [
     path('', views.index),  # 首页
+    path('login/', views.logins),  # 登录
+    path('regist/', views.regist),  # 注册
+    path('logout/', views.log_out),  # 登出
 ]
--- wechat/search/views.py
+++ wechat/search/views.py
@@ -1,9 +1,51 @@
 from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Links
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
+
 def index(request):
     linklist = Links.objects.all()
-    return render(request, 'search/index.html',{'linklist':linklist})
+    return render(request, 'search/index.html', {'linklist': linklist})
+
+
+def logins(request):
+    if request.method == 'POST':
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('/')
+        else:
+            message = "用户名或密码错误!"
+            return render(request, 'search/login.html', locals())
+    return render(request, 'search/login.html')
+
+
+def regist(request):
+    if request.method == 'POST':
+        username = request.POST.get("username")
+        password1 = request.POST.get("password1")
+        password2 = request.POST.get("password2")
+        email = request.POST.get("email")
+        if password1 != password2:
+            message1 = '两次输入的密码不一样！'
+            return render(request, 'search/regist.html', locals())
+        else:
+            if(User.objects.filter(username=username)):
+                message3 = '用户已经存在，请重新选择用户名！'
+                return render(request, 'search/regist.html', locals())
+
+        cuser = User.objects.create_user(
+            username=username, password=password1, email=email)
+        cuser.save()
+        return redirect('../login/')
+    return render(request, 'search/regist.html')
+
+def log_out(request):
+    logout(request)
+    return redirect('/')
